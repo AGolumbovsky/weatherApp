adb853d8f386dd4b9c241122c96c6d50777ea675
agolumbovsky <agolumbovsky@gmail.com>
2016-12-31 16:26:56 -0500

    https heroku issue

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(function (req, res, next) {
 */
 app.use(function (req, res, next) {
   if(req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req, url);
+    res.redirect('http://' + req.hostname + req.url);
   }
   else {
     next();
