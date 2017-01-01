c13c844ea1a62dbb51dfa33df4feac871775f6c6
agolumbovsky <agolumbovsky@gmail.com>
2016-12-31 19:58:23 -0500

    centered examples comp

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = (props) => {
     return (
-        <h3> Examples Component </h3>
+        <div>
+            <h1 className="text-cente"> Examples Component </h1>
+            <p> some example locations:  </p>
+            <ol>
+                <li>
+                    <Link to="/?location=London"> London, UK</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Washington"> Washington, DC </Link>
+                </li>
+            </ol>
+        </div>
     );
 }
 
