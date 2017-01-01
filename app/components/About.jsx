f598889f98ba199c860c1876f8afddd768a10b97
agolumbovsky <agolumbovsky@gmail.com>
2016-12-31 20:09:38 -0500

    changed About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,19 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h3> About Componenet </h3>
+        <div>
+            <h1 className="text-center"> About Componenet </h1>
+            <p> React application this is for me to learn React </p>
+            <p> Here are tools I used: </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> -- The JS library used
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map </a> -- Free weather API to search by city
+                </li>
+            </ul>
+        </div>
     )
 }
 
